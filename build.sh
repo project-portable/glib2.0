3e5d627e0d047cf11ebacecf7e0f137bbf59fdaf
Natanael <natanaelgplay@gmail.com>
2020-06-26 20:42:44 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,52 +1,19 @@
+#!/bin/bash
 
+  sudo apt build-dep gtk+2.0
 
-
-
-bash ./quick-deploy
-
-
-
-
-
-exit 0
-
-
-
-
-
-
-
-
-
-echo $GITHUB_REF
-
-# Install build dependencies
-
-sudo apt install python3-pip python3-setuptools dbus debhelper desktop-file-utils dh-exec dh-python docbook-xml docbook-xsl dpkg-dev gettext gtk-doc-tools libdbus-1-dev libelf-dev libffi-dev libmount-dev libpcre3-dev libselinux1-dev libxml2-utils linux-libc-dev pkg-config python3 python3-dbus python3-gi shared-mime-info tzdata xsltproc xterm zlib1g-dev
-
-sudo python3 -m pip install meson ninja
-
-# Download sources
-
-wget -q "https://download.gnome.org/sources/glib/2.64/glib-2.64.1.tar.xz"
-
-tar -xJf  glib-2.64.1.tar.xz
-cd glib-2.64.1
-
-# Build
-
-meson _build
-ninja -C _build
-mkdir -p ./install
-mkdir -p "build_environment"
-sudo ninja -C _build install | grep ^"Installing" | awk '{print "cp -v --parents",$4"/$(basename "$2") ./build_environment"}' | sh
-
-# Compress and  copy to workspace
-
-tar -cvzf glib-2.0.tar.gz build_environment/*
-mv glib-2.0.tar.gz $GITHUB_WORKSPACE/
-
-wget "https://raw.githubusercontent.com/probonopd/uploadtool/master/upload.sh"
-
-export REPO_SLUG="project-portable/glib2.0"
-bash upload.sh $GITHUB_WORKSPACE/glib-2.0.tar.gz
+  wget https://ftp.gnome.org/pub/gnome/sources/gtk+/2.24/gtk+-2.24.32.tar.xz
+  tar -xf gtk+-2.24.32.tar.xz
+  cd gtk+-2.24.32/
+  git cherry-pick -n 2dbf31c0ea4f17a817b931c731f8de14c278300d
+  patch -Np1 -i ../xid-collision-debug.patch
+  patch -Np1 -i ../gtk2-filechooser-icon-view.patch
+  sed -i '1s/python$/&2/' gtk/gtk-builder-convert
+  ./configure \
+    --prefix=/usr \
+    --sysconfdir=/etc \
+    --localstatedir=/var \
+    --with-xinput=yes \
+    --disable-gtk-doc
+  make
+  sudo make install 
